d4092ec3ba092ab078a8de6cef497b053c1cc30f
acerowilson@gmail.com <acerowilson@gmail.com>
2022-03-24 09:17:09 -0500

    changes to filter and send

--- filter_copia.py
+++ filter_copia.py
@@ -1,23 +1,33 @@
-#!/usr/bin/python
+#!/usr/bin/env seiscomp-python
 
 import sys,os
-HOME=os.getenv("HOME")
-sys.path.append('/%s/seiscomp3/share/gds/tools/' %HOME)
+
+sys.path.append( os.path.join(os.environ['SEISCOMP_ROOT'],'share/gds/tools/'))
 
 import seiscomp3.Core
 import seiscomp3.DataModel
 
 from lib import bulletin, filter
 
+import logging
+import logging.config
+
+logging_file = os.path.join(os.environ['SEISCOMP_ROOT'],'var/log/','gds_service_copiaEvento.log')
+logging.basicConfig(filename=logging_file, format='%(asctime)s %(message)s')
+logger = logging.getLogger("copiaEvento")
+logger.setLevel(logging.DEBUG)
+
 class copiaFilter(filter.Filter):
 
-        def filter(self, ep):
+    def filter(self, event_parameter):
+        
+                
+        event_id = event_parameter.event(0).publicID()
+        logger.info("Start filter_copia for: %s" %event_id)
 
-                b=ep.event(0).publicID()
-
-                return "%s" %(str(b))
+        return "%s" %event_id
 
 
 if __name__ == "__main__":
-        app = copiaFilter()
-        sys.exit(app())
+    app = copiaFilter()
+    sys.exit(app())
--- send_copia.py
+++ send_copia.py
@@ -1,86 +1,132 @@
-#!/usr/bin/env python
+#!/usr/bin/env seiscomp-python 
 
 import sys,os
-HOME=os.getenv("HOME")
-sys.path.append('/%s/seiscomp3/share/gds/tools/' %HOME)
 
-from lib import bulletin, logger, spooler
+sys.path.append( os.path.join(os.environ['SEISCOMP_ROOT'],'share/gds/tools/'))
+
+from lib import bulletin, spooler
 import subprocess
+from subprocess import Popen, PIPE, STDOUT
 import json
 import time
 from datetime import datetime 
 
+
+import logging
+import logging.config
+
+logging_file = os.path.join(os.environ['SEISCOMP_ROOT'],'var/log/','gds_service_copiaEvento.log')
+logging.basicConfig(filename=logging_file, format='%(asctime)s %(message)s')
+logger = logging.getLogger("copiaEvento")
+logger.setLevel(logging.DEBUG)
+
 class copiaConfig:
     def __init__(self,config):
 
-        prefix = "origen"
-        try:  self.eqeventsFolder = config.get(prefix, "eqeventsFolder")
-        except: self.eqeventsFolder="~/seiscomp3/var/lib/eqevents/pages/"
-        try:  self.serverFile = config.get(prefix, "serverFile")
-        except: self.eqeventsFolder=None
+        prefix = "eqevents"
+        try:  
+            self.eqevents_path = config.get(prefix, "eqevents_path")
+        except Exception as e: 
+            logger.error("Error reading cfg file: %s" %e)
+            self.eqevents_path="~/seiscomp/var/lib/eqevents/pages/"
+        try:  
+            self.target_server_file = config.get(prefix, "target_server_file")
+        except: 
+            self.target_server_file=None
+
 class SpoolCopia(spooler.Spooler):
 
     def __init__(self):
-
         spooler.Spooler.__init__(self)
         self._copia=copiaConfig(self._config)
-        logger.info("##Cargado el origen %s" %self._copia)
-
+        
     def _readServerFile(self,json_file):
         try:
-            print(json_file)
             with open(json_file) as json_data_files:
                 return json.load(json_data_files)
-        except Exception, e:
+        except Exception as e:
             raise Exception("##Error in _readServerFile(): %s" %str(e))
 
     def spool(self,addresses,content):
         
-        #print("##%" %addresses)
+
+        """From events folder, just the event folder must be copied
+           From browser just the last year must be copied"""
         """
-        #self.eqeventsFolder="~/seiscomp3/var/lib/eqevents/pages/"
-        #eqeventsFolder="/home/seiscomp/seiscomp3/var/lib/eqevents/pages/"
-        $SEISCOMP3/var/lib/eqevents/
+        EQEVENTS folder structure
+        $SEISCOMP_ROOT/var/lib/eqevents/
                             pages/
                                 index.html*
                                 events/
                                     igepn2018xxxa*/
                                     igepn2018xxxb/
-                                browser*/
+                                browser/*
+                                browser.html *
                                 static/
                             spool
         """
+        event_folder = '%s/events/%s' %(self._copia.eqevents_path,content.strip())
+        pages_folder='%s/' %(self._copia.eqevents_path)
+        year = content.strip()[5:-4]
+        browser_folder='%s/browser/' %(self._copia.eqevents_path)
+
+        if not os.path.exists(event_folder):
+            time.sleep(15)
+        target=self._readServerFile(self._copia.target_server_file)
         
-        srvs=self._readServerFile(self._copia.serverFile)
-        time.sleep(10)
         for a in addresses:
-            srv=a[1]
-            eventFolder='%s/events/%s' %(self._copia.eqeventsFolder,content.strip())
-            pagesFolder='%s/' %(self._copia.eqeventsFolder)
-            y=content.strip()[5:-4]
-            browserFolder='%s/browser/' %(self._copia.eqeventsFolder)
+            target_name=a[1]
             
-            """La carpeta destino debe reflejara el contenido de PAGES"""
-            destino='%s@%s:%s' %(srvs[srv]['username'],srvs[srv]['ip'],srvs[srv]['destino'])
+            """La carpeta target_path debe reflejara el contenido de PAGES"""
+            target_path='%s@%s:%s' %(target[target_name]['username'],target[target_name]['ip'],target[target_name]['target_path'])
 
             try:
                 
                 """"Copy the event folder"""
-                logger.info('#Copiando evento %s a: %s/events/' %(content.strip(), destino) )
-                subprocess.call(['sshpass -p `cat %s` scp -pr %s  %s/events/ ' %(srvs[srv]['password'],eventFolder, destino) ],shell=True)
-                logger.info("Evento %s copiado " %(content.strip()))
+                logger.info('#Copying event %s to: %s/events/' %(content.strip(), target_path) )
+                subprocess.call(['sshpass -p `cat %s` scp -pr %s  %s/events/ ' 
+                                %(target[target_name]['password'],event_folder, target_path) ],shell=True)
+                logger.info("Event %s copied " %(content.strip()))
                 
-                """ Copy the index.html file """
-                logger.info('#Copiando index.html a: %s/' %( destino) )
-                subprocess.call(['sshpass -p `cat %s` scp -pr %s/index.html %s/' %(srvs[srv]['password'],pagesFolder, destino) ],shell=True)
-                logger.info("index.html copiado ")               
-                
-                """Copy the browser pages"""
-                logger.info('#Copiando archivos browser a: %s' %(destino))
-                subprocess.call(['sshpass -p `cat %s` scp -pr %s/%s*  %s/browser/ ' %(srvs[srv]['password'],browserFolder,y, destino) ],shell=True)
-                logger.info("Carpeta browser copiado")
             except Exception as e:
-                raise Exception("##No se pudo copiar a la carpeta %s: %s" %(destino,str(e)))
+                logger.error("Could not copy %s: %s" %(target_path,str(e)))
+
+            try:
+
+                """ Copy the index.html file """
+                logger.info('#Copying index.html a: %s/' %( target_path) )
+                subprocess.call(['sshpass -p `cat %s` scp -pr %s/index.html %s/' 
+                                %(target[target_name]['password'],pages_folder, target_path) ],shell=True)
+                logger.info("index.html copied ")
+            except Exception as e:
+                logger.error("Could not copy %s: %s" %(target_path,str(e)))
+
+            try:
+                """ Copy the browser.html file """
+                logger.info('#Copying browser.html a: %s/' %( target_path) )
+                subprocess.call(['sshpass -p `cat %s` scp -pr %s/browser.html %s/' 
+                                %(target[target_name]['password'],pages_folder, target_path) ],shell=True)
+                logger.info("browser.html copied ")                
+            except Exception as e:
+                logger.error(" Could not copy %s: %s" %(target_path,str(e)))
+
+            try:
+                """ Copy the events.csv and events.xml files """
+                logger.info('#Copying events.* a: %s/' %( target_path) )
+                result = subprocess.check_output(['sshpass -p `cat %s` scp -pr %s/events.* %s/' 
+                                                %(target[target_name]['password'],pages_folder, target_path) ],shell=True)
+                logger.info("events.* copied: %s" %result)        
+            except Exception as e:
+                logger.error("Could not copy %s: %s" %(target_path,str(e)))
+            try:
+                """Copy the browser pages"""
+                logger.info('#Copying browser to: %s' %(target_path))
+                result = subprocess.check_output(['sshpass -p `cat %s` scp -pr %s/%s*  %s/browser/ ' 
+                                                %(target[target_name]['password'],browser_folder,year, target_path) ],shell=True)
+                logger.info("Folder browser copied: %s" %result)
+            
+            except Exception as e:
+                logger.error("Could not copy %s: %s" %(target_path,str(e)))
 
 
 if __name__=="__main__":
